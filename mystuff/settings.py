345035ac538e435655046678cf104adbc5083935
Pluto Yang <yangyj.ee@gmail.com>
2018-06-14 23:16:45 +0800

    missing a </div> in hist template

--- mystuff/settings.py
+++ mystuff/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'groceries.apps.GroceriesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'groceries',
 ]
 
 MIDDLEWARE = [
