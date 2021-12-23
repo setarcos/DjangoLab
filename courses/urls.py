210ef5adda3a8f842d4accf215705c963307986d
Pluto Yang <yangyj.ee@gmail.com>
2021-12-23 20:59:32 +0800

    Join and leave a group

--- courses/urls.py
+++ courses/urls.py
@@ -6,4 +6,7 @@ app_name = 'courses'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<course_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^join/(?P<group_id>[0-9]+)/$', views.joinGroup, name='joinGroup'),
+    url(r'^leave/(?P<group_id>[0-9]+)/$', views.leaveGroup, name='leaveGroup'),
+    url(r'^group/(?P<group_id>[0-9]+)/$', views.groupDetail, name='gdetail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
-from .models import Course, CourseGroup
+from django.contrib.auth.models import User
+from .models import Course, CourseGroup, StudentGroup, StudentHist
 
 def index(request):
     course_all = Course.objects.all()
@@ -10,3 +11,40 @@ def detail(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
     group = CourseGroup.objects.filter(course=course)
     return render(request, 'courses/detail.html', {'course': course, 'group': group})
+
+def groupDetail(request, group_id):
+    group = get_object_or_404(CourseGroup, pk=group_id)
+    student = StudentGroup.objects.filter(group=group).order_by('seat')
+    return render(request, 'courses/group_detail.html', {'group': student})
+
+def joinGroup(request, group_id):
+    if request.user.username != 'Student':
+        raise Http404("你必须是学生才可以选课")
+    group = get_object_or_404(CourseGroup, pk=group_id)
+    allgroup = CourseGroup.objects.filter(course=group.course)
+    for g in allgroup.all():
+        m = StudentGroup.objects.filter(group=g,stu_id = request.session['schoolid'])
+        if (m.count() > 0):
+            if (g == group):
+                raise Http404("你已经在这个组里面了")
+            else:
+                raise Http404("你在这个课的其他组里")
+    student = StudentGroup.objects.filter(group=group)
+    m = StudentGroup(group = group)
+    m.stu_id = request.session['schoolid']
+    m.stu_name = request.session['realname']
+    if (student.count() > 0):
+        m.seat = student.order_by('-seat')[0].seat + 1
+    else:
+        m.seat = 1
+    m.save()
+    return render(request, 'courses/group_detail.html', {'group': student.order_by('seat')})
+
+def leaveGroup(request, group_id):
+    group = get_object_or_404(CourseGroup, pk=group_id)
+    m = StudentGroup.objects.filter(group=group,stu_id=request.session['schoolid'])
+    if (m.count() > 0):
+        m.delete()
+    student = StudentGroup.objects.filter(group=group)
+    return render(request, 'courses/group_detail.html', {'group': student.order_by('seat')})
+
